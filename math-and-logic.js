b3f2eb56fc0dd77b8c0ad08b1741ee2ea54120d4
DanielleColucci <ddcolucci99@gmail.com>
2023-04-04 15:50:56 -0400

    add math and logic problems

---
+++ math-and-logic.js
@@ -0,0 +1,18 @@
+// 1. You have 20 bottles of pills. 19 bottles have 1.0 gram pills, but one has pills of weight 1.1 grams. Given a scale that provides an exact measurement, how would you find the heavy bottle? You can only use the scale once. 
+
+// 2. You have a basketball hoop and someone says that you can play one of two games. 
+    // Game 1: You get one shot to make the hoop
+    // Game 2: You get three shots and you have to make two of three shots
+    // If p is the probability of making a particular shot, for which values of p should you pick one game or the other?
+
+// 3. There is an 8x8 chessboard in which two diagonally opposite corners have been cut off. You are given 31 dominos, and a single domino can cover exactly two squares. Can you use the 31 dominos to cover the entire board? Provde your answer (by providing an example or showing why it is impossible)
+
+// 4. There are three ants on different vertices of a triangle. What is the probability of collision (between any two or all of them) if they start walking on the sides of the triangle? Assume that each ant randomly picks a direction, with either direction being equally likely to be chosen, and that they walk at the same speed 
+
+// 5. You have a five quart jug, a three quart jug, and an unlimited supply of water (but no measuring cups). How would you come up with exactly four quarts of water? Note that the jugs are oddle shaped, such that filling up exactly "half" of the jug would be impossible
+
+// 6. A bunch of people are living on an island, when a visitor comes with  strange order: all blue-eyed people must leave the island as soon as possible. There will be flight out at 8:00pm every evening. Each person can see everyone else's eye color, but they do not know their own (nor is anyone allowed to tell them). Additionally, they do not know how many people have blue eyes, although they do know that at least one person does. How many days will it take the blue-eyed people to leave.
+
+// 7. In the new post-apocalyptic world, the world queen is desperately concerned about the birth rate. Therefore, she decrees that all familiesd hosuld ensure that they have one girl or else they face massive fines. If all families abide by this policy -- that is, they have continued to have children until they have one girl, at which point they immediately stop -- what will the gender ratio of the new generation be? (Assume that the odds of someone having a boy or girl on any given pregnancy is equal). Solve this out logically and then write a computer simulation of it 
+
+// 8. There is a building of 100 floors. If an egg drops from the Nth floor or above, it will break. If it is dropped from any floor below, it will not break. You're given two eggs. Find N, while minimizing the number of drops for the worst case.
